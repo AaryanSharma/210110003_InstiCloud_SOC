2f2658224214c066b8347aa5451e2f0142089678
AaryanSharma <44542289+AaryanSharma@users.noreply.github.com>
2022-05-09 20:46:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ let o3El=document.getElementById("o3")
 
 let i=1
 let inputsarr=[]
+let outputarr=[]
 let checked=false
 let correct=0
 questionsarr=["What does the word 'suns' look like upside down?","snus","suns", "nothing","How many letters are in PNEUMONOULTRAMICROSCOPICSILICOVOLCANOCONIOSIS?","45","44", "36","When did Rickroll start","2000","1909","2007"]
@@ -46,14 +47,24 @@ Quiz()  }
 function Result(){
 if(inputsarr[0]=="b"){
 correct++
+outputarr.push("correct") 
 }
+else {
+outputarr.push("incorrect") }
 if(inputsarr[1]=="a"){
 correct++
+outputarr.push("correct") 
 }
+else {
+outputarr.push("incorrect") } 
 if(inputsarr[2]=="c"){
 correct++
+outputarr.push("correct") 
 }
-outputEl.innerText="Marks: "+correct+"/3"  
+else {
+outputarr.push("incorrect") }
+ 
+outputEl.innerText="Marks: "+correct+"/3"+"(1: "+outputarr[0]+ ",2: " +outputarr[1]+ ",3: " + outputarr[2]  
 if(correct===3){
 document.getElementById("rewards").style.display = "block"
 }
