60ccfbd5777d16e6aae0b596e7a1146cf7accbae
AaryanSharma <44542289+AaryanSharma@users.noreply.github.com>
2022-07-29 23:33:01 +0530

    Add files via upload

---
+++ SignUp Webpage/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Signup from "./Signup";
+
+const App = () => <Signup />;
+
+ReactDOM.render(<App />, document.getElementById("root"));
