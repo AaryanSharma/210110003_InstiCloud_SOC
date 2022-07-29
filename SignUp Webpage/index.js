e4f858c452beb3174a539142729a00c54ad994a4
AaryanSharma <44542289+AaryanSharma@users.noreply.github.com>
2022-07-29 23:35:08 +0530

    Delete SignUp Webpage directory

--- SignUp Webpage/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Signup from "./Signup";
-
-const App = () => <Signup />;
-
-ReactDOM.render(<App />, document.getElementById("root"));
